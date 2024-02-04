b13468466b80197c8ecdbb8b51944a2b8696f550
John Turner <jturner.usa@gmail.com>
2024-02-03 19:30:39 -0500

    impl strlen function

--- diamond.asm
+++ diamond.asm
@@ -1,10 +1,44 @@
-%define SYSCALL_WRITE 1
-%define SYSCALL_EXIT 60
+%define SYS_WRITE 1
+%define SYS_EXIT 60
 %define STDOUT 1
+%define NEWLINE 0xa
+
+%macro PROLOUGE 0
+        push rbp
+        mov rbp, rsp
+%endmacro
+
+%macro EPILOGUE 0
+        pop rbp
+        ret
+%endmacro
 
 section .text
 global _start
+        
 _start:
-  mov rax, SYSCALL_EXIT
-  mov rdi, 0
-  syscall
+        mov rbp, rsp
+.exit:
+        mov rax, SYS_EXIT
+        syscall
+
+strlen:
+        ; arguments:
+        ;    in -> QWORD pointer to string
+        ;    out -> QWORD length
+        PROLOUGE                ; pushes rbp, and sets rbp to rsp
+        mov rsi, [rbp + 16 + 8] ; load pointer from arguments
+        mov rax, 0              ; zero counter
+.loop:
+        cmp BYTE [rsi], 0       ; check if the end is reached
+        je .return
+        inc rax
+        inc rsi
+        jmp .loop
+.return:
+        mov [rbp + 16], rax
+        EPILOGUE
+
+; Local Variables:
+; mode: nasm
+; End:
