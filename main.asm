8ad0f71bc37fc760d4a6b91614b6a8fc93133682
John Turner <jturner.usa@gmail.com>
2024-04-07 17:08:18 -0400

    segfaults

--- main.asm
+++ main.asm
@@ -14,11 +14,34 @@
         ret
 %endmacro
 
+section .text
+        
 global _start
+extern ircd_stoi
+extern ircd_draw_row
         
 _start:
         mov rbp, rsp
+
+        ; check if argc <= 1
+        cmp QWORD [rsp], 1
+        jle .exit_failure
+       
+        ; load up diamond size and call stoi
+        mov rdi, [rsp + 16] ; argv[1]
+        mov rsi, error
+        call ircd_stoi
+        mov rdi, rax ; move return value into rdi
+        
 .exit:
         mov rax, SYS_EXIT
         syscall
 
+.exit_failure:
+        mov rax, SYS_EXIT
+        mov rdi, 1
+        syscall
+
+section .bss
+
+error: resd 1
